dc0f24b6806a92f586c74f7990d0cb74dcae45c0
wanghaisheng <edwin_uestc@163.com>
2025-05-01 03:19:41 +0800

    fix issue

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="@sanity/astro/module" />
+/// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference types="vite-plugin-pwa/vanillajs" />
 /// <reference types="vite-plugin-pwa/pwa-assets" />
